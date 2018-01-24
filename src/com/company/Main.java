ec21555a6d618cb4c22a35469cf85064d1286d69
JordanRBurke <jordanrobertburke@gmail.com>
2018-01-24 15:20:01 -0500

    Project Complete

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Math.pow(3, 5) = " + Math.pow(3,5));
+        System.out.println(10.0 / 7);
+
+        System.out.println("5 * Math.pow(2, 5) - 4 = " + (5 * Math.pow(2, 5) - 4));
+
+        System.out.println(10.3 % 5);
+        System.out.println(5536253 % 6);
+
+//        System.out.println("Math.random() = " + (int) (Math.random());
+        System.out.println("Math.ceil(Math.random() * 10 = " + Math.ceil(Math.random() * 10));
+        // Ceil rounds to the top while floor rounds to the bottom. Ceil 55.5 = 56, Floor 55.5 = 55
+
+        System.out.println("Math.round(4.5) = " + Math.round(4.5));
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
